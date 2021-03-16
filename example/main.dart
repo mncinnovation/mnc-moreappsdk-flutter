297de7d1b14cf88e83879150c39886457cd89183
Handi Deyana <handi.deyana@mncgroup.com>
2021-03-16 16:29:28 +0700

    Add Example

---
+++ example/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mncapps/mncapps.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.near_me),
+            onPressed: () {
+              //Example Full Screen MNC More Apps
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MNCAppsScreen(
+                    userID: "YOUR USER ID",
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      //Example Implemented in body of widget
+      body: MNCAppsBody(
+        userID: "YOUR USER ID",
+      ),
+    );
+  }
+}
